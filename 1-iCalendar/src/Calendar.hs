66529e9e9301d669ab503dd60b28345a930ab7dc
DatDaniel1 <daniel.vanderhoeven2004@gmail.com>
2024-11-29 23:30:20 +0100

    push

--- 1-iCalendar/src/Calendar.hs
+++ 1-iCalendar/src/Calendar.hs
@@ -4,17 +4,17 @@ module Calendar where
 
 import ParseLib
 import DateTime
-import Data.Char (isAlpha)
+import Data.Char
 import Data.List
 
 
 -- Exercise 6
-data Calendar = Calendar { calprop    :: [Calprop]
-                         , eventList  :: [Event] }
+data Calendar = Calendar { calpropList    :: [Calprop]
+                         , eventList      :: [Event] }
     deriving (Eq, Ord, Show)
 
 data Calprop
-    = ProdID String
+    = PropID String
     | Version String
     deriving (Eq, Ord, Show)
 
@@ -33,18 +33,22 @@ data EventProp
 -- Exercise 7
 newtype Token = Token { token :: String } deriving (Eq, Ord, Show)
 
+-- Divides the strings at the crlf points and takes everything that is seperated by the crlf points
 lexCalendar :: Parser Char [Token]
 lexCalendar = concatTokens . filterTokens <$> listOf (Token <$> many (satisfy (/= '\r'))) (ParseLib.token "\r\n")
 
+-- Filters any tokens that are empty out
 filterTokens :: [Token] -> [Token]
 filterTokens = filter (not . (\(Token t) -> null t))
 
+-- Concats two tokens if the second one starts with a space as those two should be one token
 concatTokens :: [Token] -> [Token]
 concatTokens ((Token t1):(Token t2):ts)
     | head t2 == ' ' = Token (t1++drop 1 t2):concatTokens ts
     | otherwise      = Token t1 : concatTokens (Token t2:ts)
 concatTokens [t] = [t]
 concatTokens []  = []
+
 
 parseCalendar :: Parser Token Calendar
 parseCalendar = pack parseStartCal parseInsideCalendar parseEndCal
@@ -58,14 +62,16 @@ parseEndCal = satisfy (\(Token t) -> t == "END:VCALENDAR")
 parseInsideCalendar :: Parser Token Calendar
 parseInsideCalendar = Calendar <$> parseCalProp <*> parseEventList
 
+
 parseCalProp :: Parser Token [Calprop]
 parseCalProp = many $ parseProdID <<|> parseVersion
 
 parseProdID :: Parser Token Calprop
-parseProdID = (\(Token t) -> ProdID $ drop 7 t) <$> satisfy (\(Token t) -> "PRODID:" `isPrefixOf` t)
+parseProdID = (\(Token t) -> PropID $ drop 7 t) <$> satisfy (\(Token t) -> "PRODID:" `isPrefixOf` t)
 
 parseVersion :: Parser Token Calprop
 parseVersion = (\(Token t) -> Version $ drop 8 t) <$> satisfy (\(Token t) -> "VERSION:" `isPrefixOf` t)
+
 
 parseEventList :: Parser Token [Event]
 parseEventList = many $ pack parseStartEvent parseEvent parseEndEvent
@@ -79,14 +85,15 @@ parseEndEvent = satisfy (\(Token t) -> t == "END:VEVENT")
 parseEvent :: Parser Token Event
 parseEvent = Event <$> many parseEventProp
 
+
 parseEventProp :: Parser Token EventProp
 parseEventProp = parseUID <<|> parseDTStamp <<|> parseDTStart <<|> parseDTEnd <<|> parseDescription <<|> parseSummary <<|> parseLocation
 
-parseUID :: Parser Token EventProp
-parseUID = (\(Token t) -> UID $ drop 4 t) <$> satisfy (\(Token t) -> "UID:" `isPrefixOf` t)
-
 parseDTStamp :: Parser Token EventProp
 parseDTStamp = (\(Token t) -> DTStamp (fst $ head $ parse parseDateTime $ drop 8 t)) <$> satisfy (\(Token t) -> "DTSTAMP:" `isPrefixOf` t)
+
+parseUID :: Parser Token EventProp
+parseUID = (\(Token t) -> UID $ drop 4 t) <$> satisfy (\(Token t) -> "UID:" `isPrefixOf` t)
 
 parseDTStart :: Parser Token EventProp
 parseDTStart = (\(Token t) -> DTStart (fst $ head $ parse parseDateTime $ drop 8 t)) <$> satisfy (\(Token t) -> "DTSTART:" `isPrefixOf` t)
@@ -103,9 +110,35 @@ parseSummary = (\(Token t) -> Summary $ drop 8 t) <$> satisfy (\(Token t) -> "SU
 parseLocation :: Parser Token EventProp
 parseLocation = (\(Token t) -> Location $ drop 9 t) <$> satisfy (\(Token t) -> "LOCATION:" `isPrefixOf` t)
 
+
 recognizeCalendar :: String -> Maybe Calendar
 recognizeCalendar s = run lexCalendar s >>= run parseCalendar
 
 -- Exercise 8
 printCalendar :: Calendar -> String
-printCalendar = undefined
+printCalendar (Calendar calprops events) = "BEGIN:VCALENDAR\r\n" ++ printCalProps calprops ++ printEvents events ++ "END:VCALENDAR\r\n"
+
+printCalProps :: [Calprop] -> String
+printCalProps = concatMap printCalProp
+
+printCalProp :: Calprop -> String
+printCalProp (PropID propid)   = "PROPID:"  ++ propid  ++ "\r\n"
+printCalProp (Version version) = "VERSION:" ++ version ++ "\r\n"
+
+printEvents :: [Event] -> String
+printEvents = concatMap printEvent
+
+printEvent :: Event -> String
+printEvent (Event eventProps) = "BEGIN:VEVENT\r\n" ++ printEventProps eventProps ++ "END:VEVENT\r\n"
+
+printEventProps :: [EventProp] -> String
+printEventProps = concatMap printEventProp
+
+printEventProp :: EventProp -> String
+printEventProp (DTStamp dt)      = "DTSTAMP:"     ++ printDateTime dt ++ "\r\n"
+printEventProp (UID id)          = "UID:"         ++ id               ++ "\r\n"
+printEventProp (DTStart dt)      = "DTSTART:"     ++ printDateTime dt ++ "\r\n"
+printEventProp (DTEnd dt)        = "DTEND:"       ++ printDateTime dt ++ "\r\n"
+printEventProp (Description dsc) = "DESCRIPTION:" ++ dsc              ++ "\r\n"
+printEventProp (Summary sum)     = "SUMMARY:"     ++ sum              ++ "\r\n"
+printEventProp (Location loc)    = "LOCATION:"    ++ loc              ++ "\r\n"
--- 1-iCalendar/src/Features.hs
+++ 1-iCalendar/src/Features.hs
@@ -6,13 +6,89 @@ import Calendar
 
 -- Exercise 9
 countEvents :: Calendar -> Int
-countEvents = undefined
+countEvents (Calendar _ events) = length events
+
 
 findEvents :: DateTime -> Calendar -> [Event]
-findEvents = undefined
+findEvents dt (Calendar _ events) = filter (isDuring dt) events
+  where
+    isDuring :: DateTime -> Event -> Bool
+    isDuring dt event =
+      case (findDTStart event, findDTEnd event) of
+        (Just l, Just h) -> betweenDT l h dt
+        _                -> False
+
+findDTStart :: Event -> Maybe DateTime
+findDTStart (Event props) = case [dt | DTStart dt <- props] of
+    [dt] -> Just dt
+    _    -> Nothing
+
+findDTEnd :: Event -> Maybe DateTime
+findDTEnd (Event props) = case [dt | DTEnd dt <- props] of
+    [dt] -> Just dt
+    _    -> Nothing    
+
+betweenDT :: DateTime -> DateTime -> DateTime -> Bool
+betweenDT l h v = (l == v && h /= v) || (intL `lowerDT` intV && intV `lowerDT` intH)
+    where 
+        intL = intListDT l
+        intH = intListDT h
+        intV = intListDT v
+          
+intListDT :: DateTime -> [Int]
+intListDT (DateTime (Date (Year dy) (Month dm) (Day dd)) (Time (Hour th) (Minute tm) (Second ts)) _) = [dy,dm,dd,th,tm,ts]
+
+lowerDT :: [Int] -> [Int] -> Bool
+lowerDT (l:ls) (h:hs) 
+  | l < h     = True
+  | l > h     = False
+  | otherwise = lowerDT ls hs 
+lowerDT _ _ = False
+
 
 checkOverlapping :: Calendar -> Bool
-checkOverlapping = undefined
+checkOverlapping cal@(Calendar _ events) = any isOverlapping events
+  where
+    isOverlapping :: Event -> Bool
+    isOverlapping event = 
+        case (findDTStart event, findDTEnd event) of
+            (Just l, Just h) -> anyOverlappingEvents l event || anyOverlappingEvents h event
+            _                -> False
 
-timeSpent :: String -> Calendar -> Int
-timeSpent = undefined
+    anyOverlappingEvents :: DateTime -> Event -> Bool
+    anyOverlappingEvents bound event = 
+        let foundEvents = findEvents bound cal
+        in  any (/= event) foundEvents
+
+
+
+timeSpent :: String -> Calendar -> [Int]
+timeSpent sum (Calendar _ events) = scanl (+) 0 (map eventMinutes (findSumEvents sum events))
+  where 
+    isSumInEvent :: String -> Event -> Bool
+    isSumInEvent sum (Event props) = case [sum' | Summary sum' <- props] of
+      [sum'] -> sum == sum'
+      _      -> False
+
+    findSumEvents :: String -> [Event] -> [Event]
+    findSumEvents sum = filter (isSumInEvent sum)
+
+    eventMinutes :: Event -> Int
+    eventMinutes event = case (findDTStart event, findDTEnd event) of
+        (Just l, Just h) -> difMinutes l h
+        _                -> 0
+
+    difMinutes :: DateTime -> DateTime -> Int
+    difMinutes l h = floor (totalMinutes h - totalMinutes l)
+    
+    -- Approximation of total time
+    totalMinutes :: DateTime -> Float
+    totalMinutes 
+      (DateTime (Date (Year dy) (Month dm) (Day dd)) (Time (Hour th) (Minute tm) (Second ts)) _)
+      = (fromIntegral dy * 365 * 24 * 60)
+      + ((fromIntegral dm-1) * 30.44 * 25 * 60)
+      + ((fromIntegral dd-1) * 24 * 60)
+      + (fromIntegral th * 60)
+      + fromIntegral tm
+      + fromIntegral ts / 60
+      
