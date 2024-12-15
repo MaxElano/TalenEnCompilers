83ea29c037e5b50a4b1a86eca9fc461a8d60e8e4
MaxElano <max.ferket2004@gmail.com>
2024-12-15 12:08:49 +0100

    Opdracht 2 "af"
    Heb denk ik wel een prima datastructuur gemaakt, maar we moeten in vervolg opdrachten maar even kijken of er iets mist/ te veel is en anders maar aanpassingen maken

--- 2-Arrow/src/Model.hs
+++ 2-Arrow/src/Model.hs
@@ -2,30 +2,64 @@ module Model where
 
 -- Exercise 1
 data Token = Token
-  | TokArrow         -- "->"
-  | TokPeriod        -- "."
-  | TokComma         -- ","
-  | TokGo            -- "go"
-  | TokTake          -- "take"
-  | TokMark          -- "mark"
-  | TokNothing       -- "nothing"
-  | TokTurn          -- "turn"
-  | TokCase          -- "case"
-  | TokOf            -- "of"
-  | TokEnd           -- "end"
-  | TokLeft          -- "left"
-  | TokRight         -- "right"
-  | TokFront         -- "front"
-  | TokSemicolon     -- ";"
-  | TokEmpty         -- "Empty"
-  | TokLambda        -- "Lambda"
-  | TokDebris        -- "Debris"
-  | TokAsteroid      -- "Asteroid"
-  | TokBoundary      -- "Boundary"
-  | TokWildcard      -- "_"
-  | TokIdent String  -- Identifier
-  | TokComment String -- Comment
+  | TokArrow         
+  | TokPeriod        
+  | TokComma         
+  | TokGo            
+  | TokTake          
+  | TokMark          
+  | TokNothing       
+  | TokTurn          
+  | TokCase          
+  | TokOf            
+  | TokEnd           
+  | TokLeft          
+  | TokRight         
+  | TokFront         
+  | TokSemicolon     
+  | TokEmpty         
+  | TokLambda        
+  | TokDebris        
+  | TokAsteroid      
+  | TokBoundary      
+  | TokWildcard      
+  | TokIdent String  
+  | TokComment String
     deriving Show
 
 -- Exercise 2
-data Program = Program deriving Show
+data Program = Program [Rule]
+    deriving Show
+
+data Rule = Rule Ident [Command]
+    deriving Show
+
+data Ident = Ident String 
+    deriving Show
+
+data Command = Command
+    | ComGo
+    | ComTake
+    | ComMark
+    | ComNothing
+    | ComTurn Direction
+    | ComCase Direction [Alt]
+    | ComIdent
+    deriving Show
+
+data Direction = DirLeft
+    | DirRight
+    | DirFront
+    deriving Show
+
+data Alt = Alt Pattern [Command]
+    deriving Show
+
+data Pattern = PatEmpty 
+    | PatLambda 
+    | PatDebris 
+    | PatAsteroid 
+    | PatBoundary 
+    | PatWildcard
+    deriving show
+
