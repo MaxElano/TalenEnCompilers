4245e2626a13d9e24eb90848586d20049fc38b05
MaxElano <max.ferket2004@gmail.com>
2024-12-15 16:44:15 +0100

    Opdracht 5 af maar nog niet getest

--- 2-Arrow/src/Algebra.hs
+++ 2-Arrow/src/Algebra.hs
@@ -4,9 +4,35 @@ import Model
 
 
 -- Exercise 5
-type Algebra = ()
-fold = undefined
+type Algebra r = 
+    ( [r] -> r --Rules to program
+    , Ident2 -> [r] -> r --Ident with commands to rule
+    , Command -> r
+    , Direction -> r 
+    , Pattern -> r 
+    , (Pattern, [r]) -> r
+    )
 
+fold :: Algebra r -> Program -> r
+fold (proAlgebra, rulAlgebra, comAlgebra, dirAlgebra, patAlgebra, altAlgebra) = doProgram
+  where
+    setProgram :: Program -> r
+    setProgram = proAlgebra . map setRule
+
+    setRule :: Rule -> r
+    setRule (Rule ident commands) = rulAlgebra ident (map setCommand commands)
+
+    setCommand :: Command -> r 
+    setCommand ComGo                      = comAlgebra ComGo
+    setCommand ComTake                    = comAlgebra ComTake
+    setCommand ComMark                    = comAlgebra ComMark
+    setCommand ComNothing                 = comAlgebra ComNothing
+    setCommand ComTurn (Direction d)      = comAlgebra (ComTurn (dirAlgebra d))
+    setCommand ComCase (Direction d) alts = comAlgebra (ComCase (dirAlgebra d) (map setAlt alts))
+    setCommand ComIdent text              = comAlgebra (ComIdent text)
+
+    setAlt :: Alt -> r 
+    setAlt (Alt pattern commands) = altAlgebra (Alt (patAlgebra pattern) (map setCommand commands))
 
 
 -- Exercise 6
