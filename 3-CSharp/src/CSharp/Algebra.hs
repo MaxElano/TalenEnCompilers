ba725eb37b752a923a854403d0748553c1959ba9
MaxElano <114808761+MaxElano@users.noreply.github.com>
2025-01-23 01:39:01 +0100

    reset

--- 3-CSharp/src/CSharp/Algebra.hs
+++ 3-CSharp/src/CSharp/Algebra.hs
@@ -7,39 +7,39 @@ import CSharp.AbstractSyntax
   Only modify this file when you change the AST in CSharpGram.hs
 -}
 
-data CSharpAlgebra c m s e env
+data CSharpAlgebra c m s e
   = CSharpAlgebra
-    { clas :: ClassName -> [m] -> env -> (c, env)
+    { clas :: ClassName -> [m] -> c
 
-    , memberD :: Decl -> env -> (m, env)
-    , memberM :: RetType -> String -> [Decl] -> s -> env -> (m, env)
+    , memberD :: Decl -> m
+    , memberM :: RetType -> String -> [Decl] -> s -> m
 
-    , statDecl   :: Decl -> env -> (s, env)
-    , statExpr   :: e -> env -> (s, env)
-    , statIf     :: e -> s -> s  -> env -> (s,env)
-    , statWhile  :: e -> s -> env -> (s, env)
-    , statReturn :: e -> env -> (s, env)
-    , statBlock  :: [s] -> env -> (s, env)
+    , statDecl   :: Decl -> s
+    , statExpr   :: e -> s
+    , statIf     :: e -> s -> s  -> s
+    , statWhile  :: e -> s -> s
+    , statReturn :: e -> s
+    , statBlock  :: [s] -> s
 
-    , exprLit   :: Literal -> env -> (e, env)
-    , exprVar   :: Ident -> env -> (e, env)
-    , exprOper  :: Operator -> e -> e -> env -> (e, env)
+    , exprLit   :: Literal -> e
+    , exprVar   :: Ident -> e
+    , exprOper  :: Operator -> e -> e -> e
     }
 
 -- The "{..}" notation brings all fields of the algebra into scope.
 -- This means that, for example, 'memberD' in the 'fMemb' definition
 -- refers to the 'memberD' field of the given algebra.
-foldCSharp :: CSharpAlgebra c m s e env -> Class -> (c, env)
+foldCSharp :: CSharpAlgebra c m s e -> Class -> c
 foldCSharp CSharpAlgebra{..} = fClas where
-  fClas (Class      t ms)     env = (clas t (map fMemb ms), env)
-  fMemb (MemberD    d)        env = (memberD d, env ++ [d])
-  fMemb (MemberM    t m ps s) env = (memberM t m ps (fStat s), env)
-  fStat (StatDecl   d)        env = (statDecl d, env ++ [d])
-  fStat (StatExpr   e)        env = (statExpr (fExpr e), env)
-  fStat (StatIf     e s1 s2)  env = (statIf (fExpr e) (fStat s1) (fStat s2), env)
-  fStat (StatWhile  e s1)     env = (statWhile (fExpr e) (fStat s1), env)
-  fStat (StatReturn e)        env = (statReturn (fExpr e), env)
-  fStat (StatBlock  ss)       env = (statBlock (map fStat ss), env)
-  fExpr (ExprLit    lit)      env = (exprLit lit, env)
-  fExpr (ExprVar    var)      env = (exprVar var, env)
-  fExpr (ExprOper   op e1 e2) env = (exprOper op (fExpr e1) (fExpr e2), env)
+  fClas (Class      t ms)      = clas t (map fMemb ms)
+  fMemb (MemberD    d)         = memberD d
+  fMemb (MemberM    t m ps s)  = memberM t m ps (fStat s)
+  fStat (StatDecl   d)         = statDecl d, env ++ [d]
+  fStat (StatExpr   e)         = statExpr (fExpr e)
+  fStat (StatIf     e s1 s2)   = statIf (fExpr e) (fStat s1) (fStat s2)
+  fStat (StatWhile  e s1)      = statWhile (fExpr e) (fStat s1)
+  fStat (StatReturn e)         = statReturn (fExpr e)
+  fStat (StatBlock  ss)        = statBlock (map fStat ss)
+  fExpr (ExprLit    lit)       = exprLit lit
+  fExpr (ExprVar    var)       = exprVar var
+  fExpr (ExprOper   op e1 e2)  = exprOper op (fExpr e1) (fExpr e2)
